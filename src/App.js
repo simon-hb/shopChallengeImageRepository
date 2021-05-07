31ef676c3a312af2bf0eceeaef97f24c2ffd55ae
SImon Jung <simon.hb.jung@hotmail.com>
2021-05-07 19:20:34 -0400

    image repository completed, need update readme

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import SearchPhotos from "./searchPhotos";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="title">Img Repo</h1>
+        <SearchPhotos />
+      </div>
+    </div>
+  );
+}
+
+export default App;
